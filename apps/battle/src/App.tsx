fe8d7f1fb3b17fc03c54b59c3d41f50309c6a036
Nam Nguyen <nam.quoine@gmail.com>
2018-09-05 18:21:07 +0700

    Init

---
+++ apps/battle/src/App.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import './App.css';
+import logo from './logo.svg';
+
+
+const AppWrapperKEKE = styled.div``
+
+
+class App extends React.Component {
+  public static displayName = "HELLO"
+  public render() {
+    return (
+      <AppWrapperKEKE>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React - BATTLE</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        </div>
+      </AppWrapperKEKE>
+    );
+  }
+}
+
+export default App;
